f54c6f7883792d5f419464af7aca373b2cfc7aa0
seadog4454 <seadog4454@gmail.com>
2020-02-11 18:16:10 +0900

    create function:read_chs

--- my_x86_OS/prog/src/boot/boot.s
+++ my_x86_OS/prog/src/boot/boot.s
@@ -15,22 +15,62 @@ ipl:
   mov %ax, %ds
   mov %ax, %es
   mov %ax, %ss
+  
   mov (.Lboot_BOOT_LOAD), %sp
   sti
-
+  
+  push %bx
+  mov $.Lboot_drive, %bx
+  mov %dl, .Lboot_drive.no(%bx)
+  pop %bx
+  push $.Lboot_s0
+  call puts
+  add $2, %sp
+ 
+ /* 
+  mov $.Lboot_BOOT_SECT, %bx
+  mov (%bx), %bx
+  sub 0x1, %bx
+  mov %bx, bx
+  #push %cx
+  #push %bx
+  #push $.Lboot_drive
+  #call read_chs
+  cmp %bx, %ax
+  jz .Lboot_10E
+  push $.Lboot_e0
+  call puts
+  add $2, %sp
+  call reboot
+.Lboot_10E:
+  jmp stage_2
+*/
 .Lboot_s0: .string "Booting...\r\n"
 .Lboot_s1: .string "--------\r\n"
 .Lboot_e0: .string "Error:sector read"
 .Lboot_BOOT_LOAD: .word 0x7C00
-
-
+.Lboot_BOOT_SIZE: .word (1024 * 8)
+.Lboot_SECT_SIZE: .word 512
+.Lboot_BOOT_SECT: .word 16 #(.Lboot_BOOT_SIZE / .Lboot_SECT_SIZE)
+/*
+.Lboot_drive:
+  .struct 0
+.Lboot_drive.no:
+  .struct .Lboot_drive.no + 2
+.Lboot_drive.cyln:
+  .struct .Lboot_drive.cyln + 2
+.Lboot_drive.head:
+  .struct .Lboot_drive.head + 2
+.Lboot_drive.sect:
+*/
 .include "../modules/real/putc.s"
 .include "../modules/real/puts.s"
-.include "../modules/real/itoa.s"
+#.include "../modules/real/itoa.s"
 .include "../modules/real/reboot.s"
-
+#.include "../modules/real/read_chs.s"
 
 /* write boot signature in 0x200 */
+
 .Lboot_boot_sig: .fill 0x1fe - (. - _start), 0x1, 0x0
 .Lboot_BOOT_SIGNATURE: .word 0xAA55
 
@@ -41,4 +81,5 @@ stage_2:
   jmp .
 
 .Lboot_s2: .string "2nd stage...\r\n"
+
 
--- my_x86_OS/prog/src/modules/real/read_chs.s
+++ my_x86_OS/prog/src/modules/real/read_chs.s
@@ -3,6 +3,10 @@
  * return: number of read sectors: ax register
  * args:
  *		drive: addr of struct drive
+ *      no: drive number
+ *      cyln: cylinder
+ *      head: head
+ *      sect: sector
  *		sect: number of sectors you want to read
  *		dst: 
  */
@@ -21,5 +25,54 @@ read_chs:
 	push %es
 	push %si
 
-	mov 0x4(%bp), %si
-	mov ()
+	mov 0x4(%bp), %si # src buff
+	
+  mov drive.clyn(%si, 0x0), %ch
+  mov drive.clyn(%si, 0x1), %cl
+  shl $6, %cl
+  or drive.sect(%si), $cl
+  
+  mov drive.head(%si), %dh
+  mov drive.no(%si), %dl
+  mov %ax, %es
+  mov 0x8(%bp), %bx # dist copy
+
+.read_chs1L
+  mov $0x2, %ah
+  mov 0x6(%bp), %al
+
+  int $0x13
+  jnc .read_chs1E
+
+  mov $0, %al
+  jmp .read_chs2E
+.read_chs1E
+  
+  cmp $0, %al
+  jne .read_chs2E
+
+  mvo $0, %ax
+  dec -0x2(%bp)
+  jnz .read_chs1L
+.read_chs2E
+  mov $0, %ah
+
+  pop %si
+  pop %es
+  pop %dx
+  pop %cx
+  pop %bx
+
+  mov %bp, %sp
+  pop %bp
+
+  ret
+
+drive:
+drive.size.start:
+drive.no: .struct 2
+drive.cyln: .struct drive.no + 2
+drive.head: .struct drive.cyln + 2
+drive.sect: .struct drive.head + 2
+drive.size.end:
+drive.size: drive.size.end - drive.size.start
